ad67c53696737de871cf296fa9b40194b4c93ac5
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-09-13 12:31:07 +0530

    feat: Add configuration for Next.js export
    This commit adds configuration options for Next.js export in the `next.config.mjs` file. The `reactStrictMode` is set to true, the output is set to 'export', and the images are set to be unoptimized. Additionally, the `assetPrefix` and `basePath` are conditionally set based on the environment.
    
    The `package.json` file is also updated to include an `export` script for Next.js export.
    
    A new workflow file `pages.yml` is added under the `.github/workflows` directory. This workflow is responsible for deploying the Next.js application to GitHub Pages.
    
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/{repository-name}' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/{repository-name}' : '',
+};
 
 export default nextConfig;
