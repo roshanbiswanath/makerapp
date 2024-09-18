49bd7d1ddfbf0e3dc991a8eca39fd79c335decb2
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-09-19 04:03:56 +0530

    refactor: Update layout and styles in AuthPage, SignUpPage and LoginPage components
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,58 +1,56 @@
 'use client';
 
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
 import { X } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 
-export default function LoginModal() {
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [isOtpSent, setIsOtpSent] = useState(false);
-  const [otp, setOtp] = useState(['', '', '', '', '', '']);
+export default function LogIn() {
+  const [loginIdentifier, setLoginIdentifier] = useState('');
 
-  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPhoneNumber(e.target.value);
-  };
-
-  const handleSendOtp = () => {
-    setIsOtpSent(true);
-  };
-
-  const handleOtpChange = (index: number, value: string) => {
-    const newOtp = [...otp];
-    newOtp[index] = value;
-    setOtp(newOtp);
-
-    if (value && index < 5) {
-      const nextInput = document.getElementById(`otp-${index + 1}`);
-      nextInput?.focus();
-    }
-  };
-
-  const handleContinue = () => {
-    console.log('Verifying OTP:', otp.join(''));
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Handle login logic here
+    console.log('Login with:', loginIdentifier);
   };
 
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4'>
-      <div className='bg-white rounded-xl w-full max-w-md relative'>
-        <Link
-          href='/'
-          className='absolute right-4 top-4'
-        >
-          <X className='h-6 w-6 text-gray-500 hover:text-gray-700' />
-        </Link>
-        <div className='p-6'>
-          <h2 className='text-2xl font-bold text-center mb-2'>Welcome back!</h2>
-          <p className='text-center text-gray-600 mb-6'>Log In to Karkhana</p>
+    <div className='min-h-screen min-w-screen bg-white mx-auto'>
+      <div className='relative h-screen w-screen flex flex-col items-center justify-center text-center'>
+        <Image
+          src='/backgroundpage.png'
+          alt='Background'
+          fill
+          className='z-0 object-cover opacity-50'
+        />
 
-          {!isOtpSent ? (
-            <>
+        <Card className='relative w-full max-w-md z-10 mx-auto rounded-xl p-6'>
+          <CardHeader className='space-y-1 text-center'>
+            <CardTitle className='text-2xl font-bold'>
+              Login to Karkhana Hub
+            </CardTitle>
+            <CardDescription className='text-xs'>
+              Empowering you to design, learn,
+              <br />
+              and make an impact.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-4 mt-6'>
+            <div className='grid grid-cols-2 gap-4'>
               <Button
                 variant='outline'
-                className='w-full mb-3 flex items-center justify-center rounded-xl p-6'
+                className='w-full rounded-full px-6 py-4 text-xs'
               >
                 <Image
                   src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg'
@@ -61,12 +59,11 @@ export default function LoginModal() {
                   height={20}
                   className='mr-2'
                 />
-                Continue with Google
+                Sign up with Google
               </Button>
-
               <Button
                 variant='outline'
-                className='w-full mb-6 flex items-center justify-center rounded-xl p-6'
+                className='w-full rounded-full px-6 py-4 text-xs'
               >
                 <Image
                   src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-original.svg'
@@ -75,91 +72,57 @@ export default function LoginModal() {
                   height={20}
                   className='mr-2'
                 />
-                Continue with LinkedIn
+                Sign Up with LinkedIn
               </Button>
-
-              <div className='flex items-center mb-6'>
-                <div className='flex-grow border-t border-gray-300'></div>
-                <span className='flex-shrink mx-4 text-gray-600'>OR</span>
-                <div className='flex-grow border-t border-gray-300'></div>
-              </div>
-
-              <div className='mb-4'>
-                <Input
-                  type='tel'
-                  placeholder='Enter your Phone Number'
-                  className='w-full rounded-xl p-6'
-                  value={phoneNumber}
-                  onChange={handlePhoneNumberChange}
-                />
-              </div>
-
-              <p className='text-xs text-gray-500 text-center mb-6'>
-                We'll call or text you to confirm your number. Standard message
-                and data rates apply. Privacy Policy
+            </div>
+            <div className='flex w-full py-2 pb-8 items-center justify-center'>
+              <Separator className='w-20 text-black' />
+              <span className='bg-background text-xs px-6 text-muted-foreground'>
+                or Sign in with
+              </span>
+              <Separator className='w-20 text-black' />
+            </div>
+            <form onSubmit={handleSubmit} className='flex flex-col items-center'>
+              <Input
+                type='text'
+                placeholder='Enter your mobile number or Email ID'
+                value={loginIdentifier}
+                onChange={(e) => setLoginIdentifier(e.target.value)}
+                className='rounded-xl mb-4 p-6'
+              />
+              <p className='text-[0.5rem] w-56 text-muted-foreground text-center mb-4'>
+                We&apos;ll text you to confirm your number. Standard message and
+                data rates apply. Privacy Policy and T&C.
               </p>
-
               <Button
-                className='w-full rounded-xl p-6 bg-gray-200 text-gray-700 hover:bg-gray-300'
-                onClick={handleSendOtp}
-              >
-                {phoneNumber ? 'Send OTP' : 'Continue'}
-              </Button>
-            </>
-          ) : (
-            <>
-              <p className='text-center text-gray-600 mb-4'>
-                Enter the code we've sent via SMS to
-              </p>
-              <p className='text-center font-semibold mb-6'>{phoneNumber}</p>
-              <div className='flex justify-between mb-6'>
-                {otp.map((digit, index) => (
-                  <div
-                    key={index}
-                    className='border-b-2'
-                  >
-                    <Input
-                      key={index}
-                      id={`otp-${index}`}
-                      type='text'
-                      maxLength={1}
-                      className='w-12 h-12 text-center text-2xl border-gray-300 rounded-xl border-none'
-                      value={digit}
-                      onChange={(e) => handleOtpChange(index, e.target.value)}
-                    />
-                  </div>
-                ))}
-              </div>
-              <Button
-                variant='link'
-                className='w-full mb-6 text-gray-500 hover:text-gray-700'
-                onClick={() => setIsOtpSent(false)}
-              >
-                Send code again
-              </Button>
-              <Button
-                className='w-full mb-4 rounded-xl p-6'
-                onClick={handleContinue}
+                type='submit'
+                className='rounded-full px-10 py-4'
               >
                 Continue
               </Button>
-              <div className='flex justify-between text-sm text-gray-500'>
-                <Button
-                  variant='link'
-                  className='p-0'
-                >
-                  Send on Whatsapp
-                </Button>
-                <Button
-                  variant='link'
-                  className='p-0'
-                >
-                  Request a call
-                </Button>
-              </div>
-            </>
-          )}
-        </div>
+            </form>
+          </CardContent>
+          <CardFooter>
+            <p className='text-sm text-center w-full'>
+              Don&apos;t have an account?{' '}
+              <Link
+                href='/signup'
+                className='underline font-medium'
+              >
+                SignUp
+              </Link>
+            </p>
+          </CardFooter>
+          <Button
+            variant='ghost'
+            size='icon'
+            className='rounded-full absolute top-1 right-1'
+          >
+            <Link href='/home'>
+              <X className='h-4 w-4' />
+            </Link>
+          </Button>
+        </Card>
       </div>
     </div>
   );
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,32 +1,66 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import { X } from 'lucide-react';
 
 export default function AuthPage() {
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
-      <div className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
-        <h1 className='text-2xl font-bold text-center mb-6'>
-          Login to Karkhana
-        </h1>
-        <p className='text-center text-gray-600 mb-6'>
-          Welcome to Karkhana. Please log in to access your account.
-        </p>
-        <Link
-          href='/auth/login'
-          passHref
-        >
-          <Button className='w-full mb-4'>Log In</Button>
-        </Link>
-        <p className='text-center text-sm text-gray-500'>
-          Don't have an account?{' '}
+    <div className='min-h-screen min-w-screen bg-white mx-auto'>
+      <div className='relative h-screen w-screen flex flex-col items-center justify-center text-center'>
+        <Image
+          src='/backgroundpage.png'
+          alt='Background'
+          fill
+          className='z-0 object-cover opacity-50'
+        />
+        <div className='relative bg-white z-10 p-8 rounded-xl shadow-md w-full max-w-md'>
           <Link
-            href='/auth/signup'
-            className='text-blue-500 hover:underline'
+            href='/'
+            className='mb-8 text-2xl font-bold flex items-center justify-center'
           >
-            Sign up
+            <Image
+              src='/Karkhana-logo.png'
+              alt='Karkhana Logo'
+              width={200}
+              height={120}
+            />
           </Link>
-        </p>
+          <p className='text-center text-gray-600 text-sm'>
+            Welcome to Karkhana.
+          </p>
+          <p className='text-center text-gray-600 text-sm'>
+            Please log in to access your account.
+          </p>
+          <Link
+            href='/auth/login'
+            passHref
+          >
+            <Button className='my-8 rounded-full px-20'>Log In</Button>
+          </Link>
+          <p className='text-center text-sm text-gray-500'>
+            Don&apos;t have an account?{' '}
+            <Link
+              href='/auth/signup'
+              className='text-blue-500 underline'
+            >
+              Sign up
+            </Link>
+          </p>
+          <Button
+            variant='ghost'
+            size='icon'
+            className='rounded-full absolute top-1 right-1'
+          >
+            <Link href='/home'>
+              <X className='h-4 w-4' />
+            </Link>
+          </Button>
+        </div>
       </div>
+      <div className='pointer-events-none absolute inset-y-0 left-0 w-full bg-gradient-to-r from-black/40 dark:from-background'></div>
+      <div className='pointer-events-none absolute inset-y-0 right-0 w-full bg-gradient-to-l from-black/40 dark:from-background'></div>
+      <div className='pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-black/40 dark:from-background'></div>
+      <div className='pointer-events-none absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-black/40 dark:from-background'></div>
     </div>
   );
 }
