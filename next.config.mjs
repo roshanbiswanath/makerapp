b1003d5b7ec11ebfb0d468b68047eecdd7b26bdc
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-10-22 12:00:36 +0530

    feat: add next-transpile-modules for MongoDB support in Next.js configuration
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import withTM from 'next-transpile-modules';
+
+const withTranspileModules = withTM({
+  transpileModules: ['mongodb'],
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withTranspileModules({
   reactStrictMode: true,
   output: 'standalone',
-};
+});
 
 export default nextConfig;
