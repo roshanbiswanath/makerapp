45791697d3bd5fa92625a95c947af929656737d7
roshanbiswanath <roshanbiswanathpatra@gmail.com>
2024-10-29 15:04:37 +0530

    Revert "feat: add next-transpile-modules for MongoDB support in Next.js configuration"
    This reverts commit b1003d5b7ec11ebfb0d468b68047eecdd7b26bdc.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-import withTM from 'next-transpile-modules';
-
-const withTranspileModules = withTM({
-  transpileModules: ['mongodb'],
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withTranspileModules({
+const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-});
+};
 
 export default nextConfig;
