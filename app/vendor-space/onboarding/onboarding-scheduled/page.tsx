3b30a62932abde41569a64401edc1e9f6d488ac4
roshanbiswanath <roshanbiswanathpatra@gmail.com>
2024-12-19 02:16:35 +0530

    feat: add onboarding scheduled page and update router import in uniqueId page

---
+++ app/vendor-space/onboarding/onboarding-scheduled/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const OnboardingScheduled = () => {
+  return (
+    <>
+    Onboarding Scheduled
+    </>
+  );
+};
+
+export default OnboardingScheduled;
