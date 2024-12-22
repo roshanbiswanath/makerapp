48f70deeda4044fc441cff41af35b5f2523ee9cf
roshanbiswanath <roshanbiswanathpatra@gmail.com>
2024-12-23 04:36:03 +0530

    feat: wrap children in Suspense for improved loading handling in AuthLayout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,11 +1,12 @@
 import TopBar from '@/components/top-bar';
 import Image from 'next/image';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen min-w-screen bg-white mx-auto">
       <TopBar theme="light" isBg />
+
       <div className="relative h-screen w-screen flex flex-col items-center justify-center text-center">
         <Image
           src="/assetlist.png"
@@ -13,7 +14,9 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
           fill
           className="z-0 object-cover opacity-50"
         />
-        {children}
+        <Suspense>
+          {children}
+        </Suspense>
       </div>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-full bg-gradient-to-r from-black/50 backdrop-blur-lg dark:from-background"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-full bg-gradient-to-l from-black/50 backdrop-blur-lg dark:from-background"></div>
