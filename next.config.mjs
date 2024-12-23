e2924ed60594bc51ec1ccedfd427a26915771ed8
roshanbiswanath <roshanbiswanathpatra@gmail.com>
2024-12-23 11:06:57 +0530

    fix: update API destination path in next.config.mjs to include '/api'

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         source: '/api/:path*',
         // destination: 'http://localhost:3001/api/:path*',
         // destination: 'https://makerappbackend.onrender.com/api/:path*',
-        destination:"https://geek-occurrence-editorial-detail.trycloudflare.com/:path*",
+        destination:"https://geek-occurrence-editorial-detail.trycloudflare.com/api/:path*",
       },
     ];
   },
